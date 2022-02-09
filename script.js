967fe1b4094c61fc98cd0d6f1e5132bcf7b44695
Mason Fibkins <mason.fibkins@gmail.com>
2022-02-09 10:08:57 -0700

    fixed loan hover

--- script.js
+++ script.js
@@ -79,7 +79,7 @@
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [28.4, -20]
+                            'coordinates': [28.4, -19.7]
                         }
                     }
 
@@ -272,6 +272,31 @@
         });
 
         map.on('mouseleave', 'cow', () => {
+            map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+
+        map.on('mouseenter', 'loan', (e) => {
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+
+            // Copy coordinates array.
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const description = e.features[0].properties.description;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup.setLngLat(coordinates).setHTML(description).addTo(map);
+        });
+
+        map.on('mouseleave', 'loan', () => {
             map.getCanvas().style.cursor = '';
             popup.remove();
         });
